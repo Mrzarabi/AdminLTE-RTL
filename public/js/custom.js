a72e6b65ee64eedc262bb9f7c2c7fb7dab9b1239
Mrzarabi <m.hadi.z.1997@gmail.com>
2020-10-05 20:46:09 +0330

    add new js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,12 @@
-document.getElementsByClassName('custom_click').addEventListener('click', customClick);
-
-function customClick() {
-    className = "extra_custom_click";
+// This function is use in /partials/navbar/navbar
+function navMenuToggle() {
+    var el = document.getElementById('custom-click');
+    if(el.classList.contains('active_nav_toggle')){
+        el.classList.remove('active_nav_toggle');
+        el.classList.add('remove_active_nav_toggle');
+    } else {
+        el.classList.remove('remove_active_nav_toggle');
+        el.classList.add('active_nav_toggle');
+    }
 }
+// End
